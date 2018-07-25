1e705bc0310d5fb391ec411dbf4f03955b89995e
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-07-25 17:20:44 +0200

    Create graphiques.R

---
+++ R/graphiques.R
@@ -0,0 +1,16 @@
+graphiques <-
+function(){
+  
+  c("ggplotgui", "svDialogs")->packages
+  try(lapply(packages, library, character.only=T), silent=T)->test2
+  if(class(test2)== "try-error") return(ez.install())
+  data<-choix.data(nom=TRUE)
+  if(length(data)==0) return(easieR())
+  nom<-data[[1]]
+  data<-data[[2]]
+  
+  msgBox("Ne pas oublier de fermer la fenêtre htmlt (firexfox, chrome, internet explorer...) pour revenir à la session R")
+   print(ref1(packages))
+  ggplot_shiny(dataset=data)
+ 
+}
