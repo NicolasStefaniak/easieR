f8a03d4cabe24f05f8ad4efecb56e6387a529204
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-07-26 22:48:33 +0200

    Update regressions.R

--- R/regressions.R
+++ R/regressions.R
@@ -144,7 +144,7 @@ function(data=NULL, modele=NULL, Y=NULL, X_a=NULL, X_i=NULL, outlier=NULL, inf=T
       cont<-variables[which(sapply(data[,variables],class)!="factor")]
       Resultats$"Normalité multivariée"<-.normalite(data=data, X=cont, Y=NULL)
       vif(lm.r1)->FIV # calcul du facteur d inflation de la variance 
-      Resultats$"Tests de multicolinéarité"$Tests<-data.frame(Tolérance=round(1/FIV,4) , FIV= round(FIV,4))
+      Resultats$"Tests de multicolinéarité"$Tests<-data.frame("Tolérance"=round(1/FIV,4) , FIV= round(FIV,4))
       Resultats$"Tests de multicolinéarité"$Information<-"FIV : facteur d'inflation de la variance"
       dwt(lm.r1, simulate=TRUE, method= "normal", reps=500)->DWT.results
       Resultats$"Test de Durbin-Watson - autocorrélations"<-round(data.frame(Autocorrélation=DWT.results[1],"statistique de D-W"=DWT.results[2],"valeur p"=DWT.results[3]),4)->DWT.results
