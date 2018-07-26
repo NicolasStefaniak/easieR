159aa599ed6eaebf66508ad8506d559ceae48785
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-07-26 09:11:45 +0200

    Update voir.R

--- R/voir.R
+++ R/voir.R
@@ -1,11 +1,18 @@
 voir <-
 function(){
+  require("svDialogs")
   data <- Filter( function(x) 'data.frame' %in% class( get(x) ), ls(envir=.GlobalEnv) )
-  data<-dlgList(data, multiple = TRUE, title="Choix du dataframe")$res
+  data<-dlgList(data, multiple = TRUE, title=voir.msg())$res
   if(length(data)==0) return(donnees())
   get(data)->data
   for(i in 1:ncol(data)) {
   	if(class(data[,i])!="factor"){
   	attributes(data[,i])<-NULL}}
   View(data)
+}
+
+voir.msg<-function(){
+if(grepl("French",Sys.setlocale())) msg<-"Choix du dataframe" else msg<-"Choose dataframe"
+
+return(msg)
 }
