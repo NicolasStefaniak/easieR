a078c2dd01b89b2c590c5f2176123d9464336615
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-08-20 21:07:36 +0200

    Update blank.data.R

--- R/blank.data.R
+++ R/blank.data.R
@@ -8,7 +8,7 @@ blank.data <-
     if(class(test2)=="try-error") {try(install.packages("RGtk2Extras"),silent=T)->test2
       if(class(test2)=="try-error") msgBox("Desole, vous devez utiliser une version 3.4 ou superieure de R pour utiliser cette fonction")
     }
-    fichier <- dlgInput("Quel nom voulez-vous donner à vos donnees ?", "data1")$res
+    fichier <- dlgInput("Quel nom voulez-vous donner a vos donnees ?", "data1")$res
     if(length(fichier)==0) return(imp.exp())
     fichier <- strsplit(fichier, ":")
     fichier <- tail(fichier[[1]],n=1)
