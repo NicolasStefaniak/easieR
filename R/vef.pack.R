10485a63f4c4e0eb21e086540310530db83d57e6
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-08-20 15:25:24 +0200

    Update vef.pack.R

--- R/vef.pack.R
+++ R/vef.pack.R
@@ -1,10 +1,80 @@
 vef.pack <-
 function(){
-    pack.to.inst <- c("afex", "akima",  "Amelia", "asbio","BayesFactor", "bibtex","car", "cobs", "corpcor", "DAAG","deldir", "DescTools","devtools", "doBy","dplyr", "epitools", "emmeans",
-                      "foreign", "ggplot2", "ggplotgui","gmodels", "GPArotation", "gsl","knitr"  ,"lars", "lsr", "MBESS", "mc2d","mgcv", "mlogit", "nFactors", "nortest", 
-                      "outliers","pander", "pgirmess", "phia", "pkgmaker", "plyr", "ppcor", "psych", "pwr", "QuantPsyc", "quantreg", "Rcpp", "readxl", "Rfit", 
-                      "reshape2", "rmarkdown", "rms", "robust", "robustbase", "rtf", "rrcov","rmarkdown", "scatterplot3d","semPlot", "sos", "sp", "stringi", "stringr", "svDialogs", "TeachingDemos",
-                      "trimcluster", "wle", "WRS","WRS2")
+    pack.to.inst <- c('afex',
+'akima',
+'Amelia',
+'asbio',
+'BayesFactor',
+'bibtex',
+'car',
+'cobs',
+'corpcor',
+'DAAG',
+'deldir',
+'DescTools',
+'doBy',
+'dplyr',
+'emmeans',
+'epitools',
+'foreign',
+'ggplot2',
+"ggplotgui",
+'gmodels',
+'GPArotation',
+'gsl',
+'knitr',
+'lars',
+'lsr',
+'MBESS',
+'mc2d',
+'mgcv',
+'mlogit',
+'nFactors',
+'nortest',
+'olsrr',
+'outliers',
+'pander'                      
+'pgirmess',
+'phia',
+'pkgmaker',
+'plyr',
+'ppcor',
+'psych',
+'pwr',
+'QuantPsyc',
+'quantreg',
+'Rcpp',
+'readxl',
+'reshape2',
+'Rfit',
+'RGtk2',
+'RGtk2Extras',
+'rmarkdown',
+'rms',
+'robust',
+'robustbase',
+'rpivotTable',
+'rrcov',
+'rtf',
+'scatterplot3d',
+'semPlot',
+'sos',
+'sp',
+'stringi',
+'stringr',
+'svDialogs',
+'TeachingDemos',
+'trimcluster',
+ 'WRS',                   
+'WRS2'
+)
+
+
+        
+        
+        
+        
+        )
 
           list()->Resultats
     Resultats$"packages.installés.correctement"<-pack.to.inst[ which(lapply(pack.to.inst, require, character.only=T)==TRUE) ]
