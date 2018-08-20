4baf06be22c5a44ddb47a5406af74105dcea840b
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-08-20 19:52:44 +0200

    Update choix.reg.R

--- R/choix.reg.R
+++ R/choix.reg.R
@@ -1,15 +1,16 @@
 choix.reg <-
-function(){
-  try(library(svDialogs), silent=T)->test2
-  if(class(test2)== "try-error") return(ez.install())
-  
-  dlgList(c("Régressions", 
-            "Effets de mediation", 
-            "Régressions logistiques"), preselect="Régressions", multiple = FALSE, title="Quel type de régression ?")$res->choix
-  if(length(choix)==0) return(analyse())
-  if(choix=="Régressions") regressions()->Resultats
-  if(choix=="Effets de mediation") ez.mediation()->Resultats
-  if(choix=="Régressions logistiques") regressions.log()->Resultats
-  return(Resultats)
-  
-}
+  function(){
+    try(library(svDialogs), silent=T)->test2
+    if(class(test2)== "try-error") return(ez.install())
+    
+    dlgList(c("Regressions", 
+              "Effets de mediation", 
+              "Regressions logistiques"), preselect="Regressions", multiple = FALSE, title="Quel type de regression ?")$res->choix
+    if(length(choix)==0) return(analyse())
+    if(choix=="Regressions") regressions()->Resultats
+    if(choix=="Effets de mediation") ez.mediation()->Resultats
+    if(choix=="Regressions logistiques") regressions.log()->Resultats
+    return(Resultats)
+    
+  }
+
