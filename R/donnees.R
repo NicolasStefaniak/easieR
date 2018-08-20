ee20e66ba1855ee4de745bacd9fe0597fe01cecb
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-08-20 20:32:14 +0200

    Update donnees.R

--- R/donnees.R
+++ R/donnees.R
@@ -1,15 +1,15 @@
 donnees <-
-function(){options (warn=-1)
-  require(svDialogs)
-  dlgList(c("importer des données","nouveau set de données", "voir des données", "importer des résultats","Exporter des données",
-            "Générer un rapport"), preselect=NULL, multiple = FALSE, 
-          title="Quelle analyse voulez-vous réaliser?")$res->choix
-  if(length(choix)==0) return(easieR())
-  if(choix=="nouveau set de données") blank.data()->Resultats
-  if(choix=="voir des données") voir()->Resultats
-  if(choix=="importer des résultats") import.results()->Resultats
-  if(choix=="importer des données") import()->Resultats
-  if(choix=="Exporter des données") exporterD()->Resultats
-  if(choix=="Générer un rapport") ez.html(ez.results)
-  return(Resultats)
-}
+  function(){options (warn=-1)
+    require(svDialogs)
+    dlgList(c("importer des donnees","nouveau set de donnees", "voir des donnees", "importer des resultats","exporter des donnees",
+              "generer un rapport"), preselect=NULL, multiple = FALSE, 
+            title="Quelle analyse voulez-vous realiser?")$res->choix
+    if(length(choix)==0) return(easieR())
+    if(choix=="nouveau set de donnees") blank.data()->Resultats
+    if(choix=="voir des donnees") voir()->Resultats
+    if(choix=="importer des resultats") import.results()->Resultats
+    if(choix=="importer des donnees") import()->Resultats
+    if(choix=="exporter des donnees") exporterD()->Resultats
+    if(choix=="generer un rapport") ez.html(ez.results)
+    return(Resultats)
+  }
