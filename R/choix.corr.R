9814bccac6c9f32d4f40522f94314669769209ae
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-08-20 19:50:57 +0200

    Update choix.corr.R

--- R/choix.corr.R
+++ R/choix.corr.R
@@ -1,23 +1,23 @@
 choix.corr <-
-function(){options (warn=-1) 
-  c( "svDialogs")->packages
-  if(any(lapply(packages, require, character.only=T))==FALSE)  {install.packages(packages) 
-    require(packages)} 
-  writeLines("l'analyse détaillée permet d'avoir les statistiques descriptives, les tests de normalité, le nuage de points,
-\n des statistiques robustes, l'ensemble des coefficients de corrélations. 
-\n la matrice de corrélation permet de contrôler l'erreur de 1e espèce et est adaptée pour un grand nombre de corrélations
-\n la comparaison de corrélations permet de comparer 2 corrélations dépendantes ou indépendantes
-\n Le choix + autre correlations + permet d'avoir les correlation tétrachoriques et polychoriques")
-  dlgList(c("Analyse détaillee (Bravais Pearson/Spearman/tau) pour une ou peu de corrélations", 
-            "Matrice de corrélations", 
-            "Comparaison de deux corrélations",
-            "Autres corrélations"), preselect=NULL, multiple = FALSE, title="Quelle analyse voulez-vous?")$res->choix
-  if(length(choix)==0) return(analyse())
-  switch(choix,
-         "Analyse détaillee (Bravais Pearson/Spearman/tau) pour une ou peu de corrélations"=corr.complet()->Resultats,
-         "Matrice de corrélations"= corr.matrice()->Resultats,
-         "Comparaison de deux corrélations"= comp.corr()->Resultats,
-         "Autres corrélations"= tetrapoly()->Resultats
-  )
-  return(Resultats)
-}
+  function(){options (warn=-1) 
+    c( "svDialogs")->packages
+    if(any(lapply(packages, require, character.only=T))==FALSE)  {install.packages(packages) 
+      require(packages)} 
+    writeLines("l'analyse detaillee permet d'avoir les statistiques descriptives, les tests de normalite, le nuage de points,
+               \n des statistiques robustes, l'ensemble des coefficients de correlations. 
+               \n la matrice de correlation permet de contrôler l'erreur de 1e espece et est adaptee pour un grand nombre de correlations
+               \n la comparaison de correlations permet de comparer 2 correlations dependantes ou independantes
+               \n Le choix + autre correlations + permet d'avoir les correlation tetrachoriques et polychoriques")
+    dlgList(c("Analyse detaillee (Bravais Pearson/Spearman/tau) pour une ou peu de correlations", 
+              "Matrice de correlations", 
+              "Comparaison de deux correlations",
+              "Autres correlations"), preselect=NULL, multiple = FALSE, title="Quelle analyse voulez-vous?")$res->choix
+    if(length(choix)==0) return(analyse())
+    switch(choix,
+           "Analyse detaillee (Bravais Pearson/Spearman/tau) pour une ou peu de correlations"=corr.complet()->Resultats,
+           "Matrice de correlations"= corr.matrice()->Resultats,
+           "Comparaison de deux correlations"= comp.corr()->Resultats,
+           "Autres correlations"= tetrapoly()->Resultats
+    )
+    return(Resultats)
+  }
