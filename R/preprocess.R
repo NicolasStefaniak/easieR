76c16d2839b3c17a6f9e6104a71c259ec592c34f
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-08-24 09:08:17 +0200

    Update preprocess.R

--- R/preprocess.R
+++ R/preprocess.R
@@ -2,7 +2,8 @@ preprocess <-
   function(){
     choix<-dlgList(c("Rangs", "Imputation de valeurs manquantes",
                      "Selectionner des observations","Selectionner des variables","Centrer / centrer reduire","Trier",
-                     "Operations mathematiques sur des variables","Tableau croise dynamique"), multiple = F, preselect="rangs", title="Que voulez-vous faire ?")$res
+                     "Operations mathematiques sur des variables","Tableau croise dynamique",
+                    "Format large au format long"), multiple = F, preselect="rangs", title="Que voulez-vous faire ?")$res
     if(length(choix)==0) return(easieR())
     switch(choix, "Rangs"= ez.rank()->Resultats,
            "Imputation de valeurs manquantes"=ez.imp()->Resultats,
@@ -10,6 +11,7 @@ preprocess <-
            "Selectionner des variables"=SelectionV()->Resultats,
            "Centrer / centrer reduire"= Centrer.red()->Resultats,"Trier"= trier()->Resultats,
            "Operations mathematiques sur des variables"= maths()->Resultats,
+           "Format large au format long"=ez.reshape()->Resultats,
            "Tableau croise dynamique"={ 
              try(library("rpivotTable"), silent=T)->test2
              if(class(test2)== "try-error") return(ez.install())
