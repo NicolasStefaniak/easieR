4ab13f1d859a655aacc3cab74b9ecea92f8d6bb5
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-09-22 11:36:36 +0200

    Update ez.anova.R

--- R/ez.anova.R
+++ R/ez.anova.R
@@ -67,11 +67,11 @@ ez.anova<-function(data=NULL, DV=NULL, between=NULL, within=NULL,id=NULL, cov=NU
     
     if(any(outlier %in% c("Identification des valeurs influentes","id","Identification of outliers" ))) Resultats[[.ez.anova.msg("title", 13)]]<-influentes
     if(any(outlier %in% c( "Donnees sans valeur influente",  "Dataset with outliers removed","removed" ))){
-      print(!is.null(influentes$"observations influentes"[,id]))
-      print(influentes$"observations influentes"[,id])
+#      print(!is.null(influentes$"observations influentes"[,id]))
+#      print(influentes$"observations influentes"[,id])
       
       if(!is.null(influentes$"observations influentes"[,id])){
-        setdiff(data[,id],influentes$"observations influentes"[,id])->diffs
+        setdiff(data[,as.character(id)],influentes$"observations influentes"[,as.character(id)])->diffs
         print(diffs)
         data[which(data[,id] %in% diffs), ]->nettoyees
         factor(nettoyees[,id])->nettoyees[,id]
