5daf3a1dc1de06b4c3b31b81945d90bc1ec5deee
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-10-07 22:44:45 +0200

    Update ez.anova.R

--- R/ez.anova.R
+++ R/ez.anova.R
@@ -19,7 +19,7 @@ ez.anova<-function(data=NULL, DV=NULL, between=NULL, within=NULL,id=NULL, cov=NU
   # contrast = list. The names in the list corresponds to the names of the factors and the values is a matrix of coefficients for the contrasts. "pairs" or "none" are also possible
   # p.adjust = adjust p values for multiples comparisons. see <code>p.adjust</code>
   packages<-c("BayesFactor", "car","afex", "DescTools","emmeans","ggplot2","nortest", "outliers", "PMCMRplus",
-              "pgirmess",  "psych", "reshape2", "svDialogs",  "WRS", "WRS2" )
+              "pgirmess",  "psych", "reshape2", "sjstats", "svDialogs",  "WRS", "WRS2" )
   test2<-try(lapply(packages, library, character.only=T), silent=T)
   if(class(test2)== "try-error") return(ez.install())
   .e <- environment()
@@ -761,7 +761,7 @@ ez.anova<-function(data=NULL, DV=NULL, between=NULL, within=NULL,id=NULL, cov=NU
     if(grepl("French",Sys.setlocale()) | grepl("fr",Sys.setlocale())){
       names(aov.out3)<-c("ddl.num", "ddl.denom", "CME", "F", ES, "valeur.p" )
     }
-    
+    aov.out3$omega.2<-omega_sq(aov.out$aov)
     Resultats[[.ez.anova.msg("title",37)]]<- aov.out3
     if(!is.null(within) && any( sapply(data[,c(unlist(within))],nlevels)>2)) {
       GG.HF<-data.frame(round(aov.out2$pval.adjustments,5))
