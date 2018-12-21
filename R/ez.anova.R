e7b41652e6735959ff83095b7f60526fe0055606
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2018-12-21 11:34:43 +0100

    Update ez.anova.R

--- R/ez.anova.R
+++ R/ez.anova.R
@@ -769,7 +769,7 @@ ez.anova<-function(data=NULL, DV=NULL, between=NULL, within=NULL,id=NULL, cov=NU
       names(aov.out3)<-c("ddl.num", "ddl.denom", "CME", "F", ES, "valeur.p" )
     }
     omega.out<-omega_sq(aov.out$aov)
-    aov.out3<-cbind(aov.out3, omega.2=omega.out[match(rownames(omega.out), test$term),2])
+    aov.out3<-cbind(aov.out3, omega.2=omega.out[match(rownames(aov.out3), omega.out$term),2])
     
     Resultats[[.ez.anova.msg("title",37)]]<- aov.out3
     if(!is.null(within) && any( sapply(data[,c(unlist(within))],nlevels)>2)) {
