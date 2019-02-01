279bd1e6c184bbe1f09e6e1289952c2ca16ede43
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2019-02-01 12:08:56 +0100

    Update blank.data.R

--- R/blank.data.R
+++ R/blank.data.R
@@ -9,7 +9,7 @@ blank.data <-
       if(class(test2)=="try-error") msgBox("Desole, vous devez utiliser une version 3.4 ou superieure de R pour utiliser cette fonction")
     }
     fichier <- dlgInput("Quel nom voulez-vous donner a vos donnees ?", "data1")$res
-    if(length(fichier)==0) return(imp.exp())
+    if(length(fichier)==0) return(donnees())
     fichier <- strsplit(fichier, ":")
     fichier <- tail(fichier[[1]],n=1)
     if(grepl("[^[:alnum:]]", fichier)) {
