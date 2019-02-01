7bb7a36ba7dac5dfea8c422051fa868d83c71905
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2019-02-01 11:43:57 +0100

    Update ez.install.R

--- R/ez.install.R
+++ R/ez.install.R
@@ -51,8 +51,6 @@ ez.install <-
                       'readxl',
                       'reshape2',
                       'Rfit',
-                      'RGtk2',
-                      'RGtk2Extras',
                       'rmarkdown',
                       'rms',
                       'robust',
@@ -72,7 +70,7 @@ ez.install <-
                       'WRS',                   
                       'WRS2'
     )
-    
+     if(Sys.info()[[1]]=="Windows"){pack.to.inst<-c(pack.to.inst,'RGtk2','RGtk2Extras'}
     # 2b. packages manquants
     pack.uninst <- pack.to.inst[!(pack.to.inst %in% rownames(installed.packages()))]
     
