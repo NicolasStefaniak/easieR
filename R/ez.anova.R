1be7b37778f73f475ff798e4f98c195a6c1d8556
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2019-04-01 15:34:36 +0200

    Update ez.anova.R

--- R/ez.anova.R
+++ R/ez.anova.R
@@ -729,6 +729,7 @@ if(reshape.data) Resultats$call.reshape<-ez.history[[length(ez.history)]][[2]]
       }
     data<-merge(x=data, y=residus, by="match")
     print(names(data))
+    print(data)
     Resultats[[.ez.anova.msg("title",31)]]<-.normalite(data=data, X="residu", Y=NULL)
     print(Resultats)
     
