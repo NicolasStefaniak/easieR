f9c8bb5dcaad6e40d1178ef0adc7d61e5c0cc033
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2019-06-04 08:11:55 +0200

    Update ez.anova.R

--- R/ez.anova.R
+++ R/ez.anova.R
@@ -812,7 +812,9 @@ if(reshape.data) Resultats$call.reshape<-as.character(ez.history[[length(ez.hist
     
     if(!is.list(contrasts) && contrasts %in% c("pairwise",  "Comparaison 2 a 2" )){
       pair<-pairs(em.out, adjust=p.adjust)
-      Resultats[[.ez.anova.msg("title",39)]]<-summary(pair)
+      pair<-summary(pair)
+      names(pair)[which(names(pair)=="p.value")]<-"valeur.p"
+      Resultats[[.ez.anova.msg("title",39)]]<-pair
     }
     
     
