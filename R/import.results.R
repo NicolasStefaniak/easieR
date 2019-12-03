37a9876ce9aa9115a9c56800a930a07092e184cf
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2019-12-03 10:19:11 +0100

    Update import.results.R

--- R/import.results.R
+++ R/import.results.R
@@ -1,13 +1,9 @@
 import.results <-
 function(){
+  require(pander)
   fichier <- try(file.choose(), silent=TRUE)
   if(class(fichier)=="try-error") return(donnees())
-  dget(fichier)->data1
-  fichier<- dlgInput("Quel nom voulez-vous donner au fichier?", "Resultats")$res
-  if(length(fichier)==0) fichier<-"data1"
-  strsplit(fichier, ":")->fichier
-  tail(fichier[[1]],n=1)->fichier
-  assign(x=fichier, value=data1, envir=.GlobalEnv)
-  Resultats<-paste("Les resultats ont ete correctement importes dans", fichier)
+  openFileInOS(fichier)
+    Resultats<-paste("Les resultats ont ete correctement importes dans", fichier)
   return(Resultats)
 }
