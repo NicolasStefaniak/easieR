ea23fee504289c130ed7ee698ba9ca363420b070
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2019-12-18 15:26:25 +0100

    Update ez.report.R

--- R/ez.report.R
+++ R/ez.report.R
@@ -1,7 +1,7 @@
 ez.report<-function(html=NULL){
   options (warn=-1)
   require(svDialogs)
-    choix<- c("html", "MS WORD")   
+  if(is.null(html)){  choix<- c("html", "MS WORD")   
   if(grepl("French",Sys.setlocale()) | grepl("fr",Sys.setlocale())) {
     
     title<-"Quel format souhaitez-vous ?"
@@ -12,6 +12,7 @@ ez.report<-function(html=NULL){
           title=title)$res
   if(length(choix)==0) return(donnees())
   if(choix=="html") html<-T else html<-F
+    }
   ez.html(ez.results, html= html)
  
 }
