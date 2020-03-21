802ebfe6faf0924bfce3f81e1ca0d9776ad441f5
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2020-03-21 15:46:18 +0100

    Update regressions.R

--- R/regressions.R
+++ R/regressions.R
@@ -323,7 +323,7 @@ regressions <-
         table[is.na(table)]<-""
         table->Resultats$"table des betas"
         if(length(pred)>1){
-          try(ols.corr<-ols_correlations(lm.r1), silent=T)
+          ols.corr<-try(ols_correlations(lm.r1), silent=T)
           if(class(ols.corr)!="try-error"){
           Resultats$"Contribution des variables au modele"<-ols.corr
           Resultats$"Graphe des variables ajoutees" <-ols_plot_added_variable(lm.r1)}
