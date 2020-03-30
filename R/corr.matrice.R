7d60f92d9199fdd6ad6e95c14ac3219f2349705f
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2020-03-30 09:44:11 +0200

    Update corr.matrice.R

--- R/corr.matrice.R
+++ R/corr.matrice.R
@@ -66,7 +66,7 @@ corr.matrice <-
         
       }
       if(choix=="Correlations partielles"){
-        msg6<-"Veuillez preciser la ou les variables a contrôler" 
+        msg6<-"Veuillez preciser la ou les variables a controler" 
         Z<-.var.type(X=Y, info=info, data=data, type="numeric", check.prod=F, message=msg6,  multiple=T, title="Variable-s a contrôler", out=c(X1,Y))
         if(is.null(Z)) {
           corr.matrice.in(X=NULL, Y=NULL, data=NULL, method=NULL, param=NULL, outlier=NULL, sauvegarde=NULL, info=T, group=NULL,
