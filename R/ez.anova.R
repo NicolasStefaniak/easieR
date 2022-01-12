9c6b477d8215ce6c71245e12c9b915aeef8b5a35
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2022-01-12 16:08:02 +0100

    Update ez.anova.R

--- R/ez.anova.R
+++ R/ez.anova.R
@@ -794,7 +794,8 @@ if(reshape.data) Resultats$call.reshape<-as.character(ez.history[[length(ez.hist
       names(aov.out3)<-c("ddl.num", "ddl.denom", "CME", "F", ES, "valeur.p" )
     }
     #omega.out<-omega_sq(aov.out$aov)
-   omega.out<- effectsize::effectsize(aov.out$aov, type = "omega")
+   try(omega.out<- effectsize::effectsize(aov.out$aov, type = "omega"))
+     if(class(omega.out)== "try-error") omega.out<-NULL
     aov.out3<-cbind(aov.out3, omega.2=omega.out[match(rownames(aov.out3), omega.out$omegasq),3])
     
     Resultats[[.ez.anova.msg("title",37)]]<- aov.out3
