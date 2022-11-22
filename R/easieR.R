51a6362433c998600ce827b2504cfbcdbc949415
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2022-11-22 16:42:26 +0100

    Update easieR.R

--- R/easieR.R
+++ R/easieR.R
@@ -660,7 +660,7 @@ ref1 <-
   packageStartupMessage(" If you are using easieR for the first time, please use the function ez.install in order to ensure that easieR will work properly.\n Si vous utilisez easieR pour la 1e fois, veuillez utiliser la fonction ez.install pour vous assurer de bon fonctionnement de easieR.")
   packageStartupMessage("Les accents / caracteres speciaux ont volontairement ete supprimes pour assurer la portabilite de easieR sur tous les ordinateurs.")
   packageStartupMessage(textVersion)
-  packageStartupMessage("Last update 04/11/2022")
+  packageStartupMessage("Last update 11/22/2022")
   packageStartupMessage("##############")
 
 }
