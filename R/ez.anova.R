9c6a2c9373b4345a9f7657545bb8ef0cc43eb870
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2023-03-30 13:13:14 +0200

    Update ez.anova.R

--- R/ez.anova.R
+++ R/ez.anova.R
@@ -791,11 +791,10 @@ if(reshape.data) Resultats$call.reshape<-as.character(ez.history[[length(ez.hist
     aov.out3<-data.frame(aov.out3)
 #    aov.out3[,6]<-round.ps(aov.out3[,6])
     if(grepl("French",Sys.setlocale()) | grepl("fr",Sys.setlocale())){
-      names(aov.out3)<-c("ddl.num", "ddl.denom", "CME", "F", ES, "valeur.p" )
+      names(aov.out3)<-c("ddl.num", "ddl.denom", "CME", "F", ES, "valeur.p", "omega2" )
     }
-
-  #  out<-effectsize::omega_squared(aov.out$Anova)
-   #  if(class(omega.out)!= "try-error") aov.out3<-cbind(aov.out3, omega.2=omega.out[match(rownames(aov.out3), omega.out$omegasq),3])
+    omega.out<-effectsize::omega_squared(aov.out$Anova)
+    aov.out3<-cbind(aov.out3, omega.2=omega.out[match(rownames(aov.out3), omega.out$Parameter),"Omega2_partial"])
     
     Resultats[[.ez.anova.msg("title",37)]]<- aov.out3
     if(!is.null(within) && any( sapply(data[,c(unlist(within))],nlevels)>2)) {
