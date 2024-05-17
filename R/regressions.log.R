ed39d5508537c3dc3573f699d4f3e28e0e276ae1
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2024-05-17 22:00:46 +0200

    Update regressions.log.R

--- R/regressions.log.R
+++ R/regressions.log.R
@@ -192,7 +192,7 @@ if(scale==T || scale==txt_center) {
                         centre<-pred2[centre]}else{centre<-NULL}
   if(!is.null(centre)){
         if(length(centre)==1) data[,centre]-mean(data[,centre],na.rm=T)->data[,centre] else{
-    sapply(X=data[,centre], fun<-function(X){X-mean(X, na.rm=T)})->data[,centre]
+    sapply(X=data[,centre], function(X){X-mean(X, na.rm=T)})->data[,centre]
   }
   
   sapply(X=data[, names(which(sapply(data[,pred2],class)!="factor"))], fun)->data[,names(which(sapply(data[,pred2],class)!="factor"))]
