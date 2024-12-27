af379fdcba608c443fd7bc4bfc462c7b35dc7e2a
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2024-12-27 14:30:04 +0100

    Update chi.R

--- R/chi.R
+++ R/chi.R
@@ -311,7 +311,7 @@ chi <-
       return(Resultats)
     }
 
-    c('svDialogs', 'epitools', 'BayesFactor', 'ggplot2')->packages
+    c('svDialogs', 'epitools', 'BayesFactor', 'ggplot2','ggmosaic)->packages
     try(lapply(packages, library, character.only=T), silent=T)->test2
     if(class(test2)== 'try-error') return(ez.install())
     .e <- environment()
