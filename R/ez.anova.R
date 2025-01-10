8b4cdcdfa75717b9e7eb8ed86ddc38df1815cb19
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2025-01-10 21:18:14 +0100

    type.v

--- R/ez.anova.R
+++ R/ez.anova.R
@@ -502,7 +502,7 @@ ez.anova<-function(data=NULL, DV=NULL, between=NULL, within=NULL,id=NULL, cov=NU
       writeLines(.ez.anova.msg("msg",2))
       return(.ez.anova.in())
     }
-  } 
+  } else type.v<-NULL
   
   # verifier s'il y a des variables en mesures répétées et les choisir
  
