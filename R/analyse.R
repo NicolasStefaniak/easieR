4e1b8a7eba511f984c11effc09a177eeb2ff7d65
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2025-12-11 12:42:36 +0100

    Refactor analysis result assignment in analyse.R

--- R/analyse.R
+++ R/analyse.R
@@ -7,12 +7,12 @@ analyse <-
               .dico[["txt_analysis_factor_component"]],
               .dico[["txt_fiability_analysis"]]), preselect=NULL, multiple = FALSE, title=.dico[["ask_analysis_type"]])$res->choix
     if(length(choix)==0) return(easieR())
-    if(choix==.dico[["txt_chi_squared"]]) chi(html=html)->Resultats
-    if(choix==.dico[["txt_student_t"]]) test.t(html=html)->Resultats
+    if(choix==.dico[["txt_chi_squared"]]) Resultats<-ez.chi(html=html)
+    if(choix==.dico[["txt_student_t"]]) Resultats<-test.t(html=html)
     if(choix==.dico[["txt_anova_ancova"]]) {
       Filter( function(x) 'aovplus' %in% class( get(x) ), ls(envir=.GlobalEnv))->nom1
       if(length(nom1)==0) {
-	      ez.anova(html=html)->Resultats
+	      Resultats<-ez.anova(html=html)
       } else {
 	      html=html
 	      dlgList(c(.dico[["txt_principal_analysis"]],
@@ -27,11 +27,11 @@ analyse <-
 	      }
       }
     }
-    if(choix==.dico[["txt_correlations"]]) choix.corr(html=html)->Resultats
-    if(choix==.dico[["txt_regressions"]]) choix.reg(html=html)->Resultats
-    #if(choix==.dico[["txt_logistic_regressions"]]) regressions.log()->Resultats
-    if(choix==.dico[["txt_analysis_factor_component"]]) factor.an(html=html)->Resultats
-    if(choix==.dico[["txt_fiability_analysis"]]) fiabilite(html=html)->Resultats
-    if(choix==.dico[["txt_descriptive_statistics"]]) stat.desc(html=html)->Resultats
+    if(choix==.dico[["txt_correlations"]]) Resultats<-choix.corr(html=html)
+    if(choix==.dico[["txt_regressions"]]) Resultats<-choix.reg(html=html)
+    #if(choix==.dico[["txt_logistic_regressions"]]) Resultats<-regressions.log()
+    if(choix==.dico[["txt_analysis_factor_component"]]) Resultats<-factor.an(html=html)
+    if(choix==.dico[["txt_fiability_analysis"]]) Resultats<-fiabilite(html=html)
+    if(choix==.dico[["txt_descriptive_statistics"]]) Resultats<-stat.desc(html=html)
     return(Resultats)
   }
