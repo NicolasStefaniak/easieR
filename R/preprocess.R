8bec1343543dc2c8f6347653ef06223d128e0d8e
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2026-03-27 15:10:15 +0100

    Update preprocess.R

--- R/preprocess.R
+++ R/preprocess.R
@@ -2,7 +2,7 @@ preprocess <-
   function(){
     choix<-dlgList(c(.dico[["txt_ranks_upper"]], .dico[["txt_imput_missing_values"]],
                      .dico[["txt_select_obs"]],.dico[["txt_select_variables"]],.dico[["txt_center_or_center_reduce"]],.dico[["txt_order"]],
-                     .dico[["txt_mathematical_operations_on_variables"]],.dico[["txt_dynamic_crossed_table"]],
+                     .dico[["txt_mathematical_operations_on_variables"]],
                     .dico[["txt_long_or_large_format"]]), multiple = F, preselect=.dico[["txt_ranks_lower"]], title=.dico[["ask_what_to_do"]])$res
     if(length(choix)==0) return(easieR())
     if (choix==.dico[["txt_ranks_upper"]])  ez.rank()->Resultats
@@ -13,10 +13,6 @@ preprocess <-
     if (choix==.dico[["txt_order"]])  trier()->Resultats
     if (choix==.dico[["txt_mathematical_operations_on_variables"]])  maths()->Resultats
     if (choix==.dico[["txt_long_or_large_format"]]) ez.reshape()->Resultats
-    if (choix==.dico[["txt_dynamic_crossed_table"]]) {
-      try(library('rpivotTable'), silent=T)->test2
-      if(class(test2)== 'try-error') return(ez.install())
-      return( rpivotTable(choix.data(nom=F)))
-    }
+
     return(Resultats)
   }
