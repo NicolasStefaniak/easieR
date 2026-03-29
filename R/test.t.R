cd4d423bdf269c89839f84419ac8f938ac973a31
NicolasStefaniak <nicolas.stefaniak@univ-reims.fr>
2026-03-29 17:36:43 +0200

    Fix calculation of r in Wilcoxon test

--- R/test.t.R
+++ R/test.t.R
@@ -534,7 +534,7 @@ test.t <-
       if(any(param=="non param")| any(param==.dico[["txt_non_parametric_test"]])) {
         WT<-wilcox.test(large$t1,large$t2, paired=T,data=data, alternative=alternative, conf.int=T, conf.level=0.95)
         if(alternative!="two.sided")  abs(qnorm(WT$p.value))->z else abs(qnorm(WT$p.value/2))->z
-        r<-z/(length(data[,X]))^0.5
+        r<-z/(nrow(large))^0.5
         Resultats$Wilcoxon<- data.frame("Wilcoxon W"=WT$statistic, txt_p_dot_val=round(WT$p.value,4), "z"=round(z,4), "r"=round(r,4),
                                         txt_ci_inferior_limit_dot=WT$conf.int[1],txt_ci_superior_limit_dot=WT$conf.int[2])
         names(Resultats$Wilcoxon)<- c("Wilcoxon W", .dico[["txt_p_dot_val"]], "z", "r", .dico[["txt_ci_inferior_limit_dot"]],.dico[["txt_ci_superior_limit_dot"]])
